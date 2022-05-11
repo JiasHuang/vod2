ea559464c7c09993f41f07e9a54b7d241786ddc0
jiashuang <jias.tw@gmail.com>
2022-05-11 14:53:41 +0800

    refine 4gtv

--- extractors/4gtv.py
+++ extractors/4gtv.py
@@ -1,14 +1,20 @@
 import re
 import subprocess
+import os
+import signal
 
 import xurl
 
 VALID_URL = r'4gtv'
 
 def getSource(url, fmt, ref):
-    cmd = 'google-chrome-stable --headless --disable-gpu --enable-logging --v=1 --virtual-time-budget=8000 --timeout=8000 \'%s\'' %(url)
-    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode('utf8')
-    m = re.search(r'https://4gtvfree-cds.cdn.hinet.net/.*', output)
-    if m:
-        return m.group(0)
-    return None
+    result = None
+    cmd = 'google-chrome-stable --headless --disable-gpu --enable-logging --v=1 --virtual-time-budget=30000 --timeout=30000 \'%s\'' %(url)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    for line in p.stdout:
+        m = re.search(r'https://4gtvfree-cds.cdn.hinet.net/.*', line.decode('utf8'))
+        if m:
+            result = m.group(0)
+            break
+    p.terminate()
+    return result
