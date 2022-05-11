0fa6d3fcec5a4ce27024b88f1dd7737fdce30744
jiashuang <jias.tw@gmail.com>
2022-05-11 15:06:35 +0800

    refine 4gtv

--- extractors/4gtv.py
+++ extractors/4gtv.py
@@ -9,10 +9,15 @@ VALID_URL = r'4gtv'
 
 def getSource(url, fmt, ref):
     result = None
-    cmd = 'google-chrome-stable --headless --disable-gpu --enable-logging --v=1 --virtual-time-budget=30000 --timeout=30000 \'%s\'' %(url)
+    cmd = 'google-chrome-stable --headless --disable-gpu --enable-logging --v=1 --virtual-time-budget=90000 \'%s\'' %(url)
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     for line in p.stdout:
-        m = re.search(r'https://4gtvfree-cds.cdn.hinet.net/.*', line.decode('utf8'))
+        l = line.decode('utf8')
+        m = re.search(r'https://4gtvfree-cds.cdn.hinet.net/.*', l)
+        if m:
+            result = m.group(0)
+            break
+        m = re.search(r'https://4gtvfreepc-mozai.4gtv.tv/.*', l)
         if m:
             result = m.group(0)
             break
---
+++ web/extractors/4gtv.py
@@ -0,0 +1,26 @@
+import re
+import json
+
+from .utils import *
+
+VALID_URL = r'4gtv'
+
+def extract(url):
+    objs = []
+    m = re.search(r'/GetVodIndex/(\w+)/', url)
+    if m:
+        vodtype = m.group(1)
+        data = json.loads(load(url))
+        for d in data['Data']:
+            link = 'https://www.4gtv.tv/{}/{}'.format(vodtype, d['VODID'])
+            title = d['Title']
+            image = d['HeadFrame']
+            objs.append(pageObj(link, title, image))
+    elif re.search(r'/GetChannelBySetId/', url):
+        data = json.loads(load(url))
+        for d in data['Data']:
+            link = 'https://www.4gtv.tv/channel_sub.html?channelSet_id=4&asset_id={}&channel_id={}'.format(d['fs4GTV_ID'], d['fnID'])
+            title = d['fsNAME']
+            image = d['fsHEAD_FRAME']
+            objs.append(entryObj(link, title, image))
+    return objs
