cad072fc395d0a13880e77a7bda125ae73a8cb09
jiashuang <jias.tw@gmail.com>
2022-09-06 16:32:47 +0800

    update

--- pi/forward-url.py
+++ pi/forward-url.py
@@ -7,9 +7,9 @@ def main():
     url = sys.argv[1]
     os.system('adb connect 192.168.0.101')
     if url.startswith('https://www.youtube.com/'):
-        os.system('adb shell am start -a android.intent.action.VIEW -d {} --activity-clear-top -i VOD'.format(url))
+        os.system('adb shell am start -a android.intent.action.VIEW -d {} --activity-clear-top'.format(url))
     else:
-        os.system('adb shell am start -a android.intent.action.VIEW -t video/mp4 -d {} --activity-clear-top -i VOD'.format(url))
+        os.system('adb shell am start -a android.intent.action.VIEW -t video/mp4 -d {} --activity-clear-top'.format(url))
     return
 
 if __name__ == '__main__':
