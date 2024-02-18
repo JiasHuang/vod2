835b8a8af88fd41bfbde935cdfed23b6a25e656a
jiashuang <jias.tw@gmail.com>
2024-02-18 10:58:26 +0800

    refine web extractor

--- web/extractors/gimy.py
+++ web/extractors/gimy.py
@@ -13,13 +13,10 @@ def extract(url):
             title = m.group(3)
             objs.append(pageObj(link, title, image))
     elif re.search(r'/vod/', url):
-        m = re.search(r'<div class="playlist.*?">(.*?)</div>', load(url), re.DOTALL|re.MULTILINE)
-        if m:
-            playlist = m.group()
-            for x in re.finditer(r'<li><a href="([^"]*)">(.*?)</a></li>', playlist):
-                link = urljoin(url, x.group(1))
-                title = x.group(2)
-                objs.append(videoObj(link, title))
+        for x in re.finditer(r'<li><a rel="follow" href="([^"]*)">(.*?)</a></li>', load(url)):
+            link = urljoin(url, x.group(1))
+            title = x.group(2)
+            objs.append(videoObj(link, title))
     return objs
 
 def search_gimy(q, start=None):
